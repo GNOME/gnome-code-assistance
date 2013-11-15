42b0fa522c910553c063ddd85a3bfacdd870bd56
Jesse van den Kieboom <jessevdk@gmail.com>
2013-11-15 12:09:27 +0100

    [backends/css] Update sass deps

--- backends/css/mkdeps.py
+++ backends/css/mkdeps.py
@@ -4,15 +4,28 @@ import subprocess, os, re
 
 perdir = {}
 
-ret = subprocess.check_output(['git', 'ls-tree', 'HEAD', '--name-only', '-r', '--', 'gems']).splitlines()
+rethead = subprocess.check_output(['git', 'ls-tree', 'HEAD', '--name-only', '-r', '--', 'gems']).decode('utf-8').splitlines()
+retindex = subprocess.check_output(['git', 'ls-files', '--', 'gems']).decode('utf-8').splitlines()
+
+ret = rethead + retindex
+seen = {}
 
 for r in ret:
+    if r in seen:
+        continue
+
+    seen[r] = True
     dname = os.path.dirname(r)
 
     if dname in perdir:
         perdir[dname].append(r)
     else:
         perdir[dname] = [r]
+
+datas = []
+
+print('if RUBY_SASS')
+print('else')
 
 for dname in perdir:
     vname = 'csssass_{0}'.format(re.sub('[/.-]', '_', dname))
@@ -21,5 +34,11 @@ for dname in perdir:
     print('{0}_DATA = \\'.format(vname))
     print("\tbackends/css/{0}".format(" \\\n\tbackends/css/".join(perdir[dname])))
     print('')
+
+    datas.append('$({0}_DATA)'.format(vname))
+
+print('endif\n')
+
+print('EXTRA_DIST += \\\n\t{0}'.format(' \\\n\t'.join(datas)))
 
 # vi:ts=4:et
